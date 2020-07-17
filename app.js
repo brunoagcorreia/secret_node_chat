ae620d2215baad7fbff2150c0664f69646da66dd
brunoagcorreia <brunocorreia@gmail.com>
2020-07-17 20:57:28 +0100

    code cleared & bugs fixed

--- app.js
+++ app.js
@@ -1,54 +1,20 @@
-// let server = require("http").Server(app);
-// let socket = require("socket.io")(server);
-
-// let server = require("http");
+// import benötigte Packages & initialisieren Server
 const express = require("express");
 let app = express();
 let server = require("http").Server(app);
 let socket = require("socket.io")(server);
 
-// server.Server(app);
-// let socket = require("socket.io")(server);
-
-// let app = express();
-// let server = require("http")
+// View Verzeichnis mit 'root' Verzeichnis verknüpfen
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-
-
-
-// var app = require('http').createServer(handler)
-// var io = require('socket.io')(app);
-// var fs = require('fs');
-
-// app.listen(80);
-
-// function handler (req, res) {
-//   fs.readFile(__dirname + '/index.html',
-//   function (err, data) {
-//     if (err) {
-//       res.writeHead(500);
-//       return res.end('Error loading index.html');
-//     }
-
-//     res.writeHead(200);
-//     res.end(data);
-//   });
-// }
-
-// io.on('connection', function (socket) {
-//   socket.emit('news', { hello: 'world' });
-//   socket.on('my other event', function (data) {
-//     console.log(data);
-//   });
-// });
-
+// Routing: Chatroom
 app.get('/', function(req, res){
   res.render("chatroom");
 });
 
+// Noch nicht erstellt
 // socket.on("helloServer", function(message){
 //   console.log("Client angemeldet" + message.name + "grüst züruck")
 // })
@@ -61,7 +27,8 @@ socket.on("connection", function(socket){
   console.log("Client angemeldet")
   socket.emit('welcome');
   socket.on("newMessage", function(obj){
-    console.log("Client " + obj.nachricht + " sagt Hi");
+    // "obj.nachricht" ist nicht erstellt
+    // console.log("Client " + obj.nachricht + " sagt Hi");
     socket.broadcast.emit("newMessage", obj)
   })
   // socket.emit("welcome");
